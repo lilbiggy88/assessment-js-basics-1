6cf00cdf558d3f71308f1419904573a3abbdc2f4
John Biggerstaff <john.r.biggerstaff@gmail.com>
2022-08-29 21:54:59 -0600

    second commit

--- sdlc.js
+++ sdlc.js
@@ -5,17 +5,22 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
-// Implementation
-// Design
-// Maintain
-// Analysis of Requirements
-// Testing & Integration
 // Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
 
 
 //////////////////PROBLEM 2////////////////////
 /*
-    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
-    The first one is done for you. 
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each 
+    variable to a definition of the step. The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements = "This is the second step where you analyze what will be needed in order to develop the software you are trying to create. It needs to be documented, actionable, measurable, testable, traceable, defined with details, and related to the overall business needs. "
+const design = 'This is the third step, in which you need to figure out the design of what you are creating for the specific person or company. This would include understanding: the business and the context of the software, what is driving the need for the software, the goals and desired end result that the software will bring, the users, how they will be using the software, their needs, and their limitations, and the techonology experience and preferences of the company or person.'
+const implementation = 'This is the fourth step, in which you start taking the ideas and build the software for the company or persons wants and needs.'
+const testingAndIntegration = 'This is the fifth step, in which you start having people test the product and gain feedback to make the product better, more user friendly, and making sure it is ready for the market.'
+const maintain = 'This is the final step, in which the software is out and you keep it running. This is where updates come from with user feedback in making the software continue to grow and be successful.'
