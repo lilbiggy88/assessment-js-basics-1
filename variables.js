66469a605ec768895aae9f6e16b6e24a1a8f942b
John Biggerstaff <john.r.biggerstaff@gmail.com>
2022-08-29 22:15:03 -0600

    initial commit

--- variables.js
+++ variables.js
@@ -1,17 +1,17 @@
 //////////////////PROBLEM 1////////////////////
 /* Create a variable called 'myName' with a value that is your name as a string. */
 
-//CODE HERE 
+let myName = 'John Biggerstaff'
 
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
-//CODE HERE
+let favNum = '7'
 
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
-//CODE HERE
+let lovesCode = 'true'
 
 //////////////////PROBLEM 4////////////////////
 /*
@@ -20,11 +20,13 @@
   If it's not, console.log('not lucky 13').
 */
 
-//CODE HERE
+if(favNum = 7){
+  console.log("That is my Favorite Number!")
+}
 
 //////////////////PROBLEM 5////////////////////
 /*
   Write a for loop that prints out your faveNum 5 times.
 */
 
-//CODE HERE
+for(if )
